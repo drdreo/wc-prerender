9555e7d9f33f6418b17fb8c6a0c9a0a389c172d1
Andreas Hahn <drdreo@users.noreply.github.com>
2020-01-24 14:51:55 +0100

    Update prerender.ts

--- headless-browser/prerender.ts
+++ headless-browser/prerender.ts
@@ -1,20 +1,27 @@
 const path = require('path');
 const fs = require('fs');
 
-import { render } from './render';
+import { render, launched, stopBrowser } from './render';
 import { rehydrate } from './hydrate';
 
 const file = '/public/pages/index.html';
-render(file)
-	.then((pageContent: string) => {
-		console.log('[Headless Browser] Pre-rendering finished!');
-		fs.writeFile(
-			path.join(__dirname, file.replace('.html', '.ssr.html')),
-			// add re-hydration script to the end of <body>
-			pageContent.replace('</body>', `${rehydrate}</body>`),
-			{encoding: 'utf8'},
-			async (err) => {
-				if (err) console.log(err);
-			},
-		);
+
+// need to wait for the browser to launch
+launched
+	.then(() => {
+		render(file)
+			.then((pageContent: string) => {
+				console.log('[Headless Browser] Pre-rendering finished!');
+				fs.writeFile(
+					path.join(__dirname, file.replace('.html', '.ssr.html')),
+					// add re-hydration script to the end of <body>
+					pageContent.replace('</body>', `${rehydrate}</body>`),
+					{ encoding: 'utf8' },
+					async (err) => {
+						if (err) console.log(err);
+					},
+				);
+
+				stopBrowser();
+			});
 	});
